5947f9eb58c7769a5bc8014e01cc9e96162bcb9c
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2024-02-15 17:01:30 -0500

    Delete Function

--- server/routes/usersApi.js
+++ server/routes/usersApi.js
@@ -9,6 +9,7 @@ const express = require("express");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const { ObjectId } = require("mongodb");
+const user = require("../models/user");
 
 const router = express.Router();
 
