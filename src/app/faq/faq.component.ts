2e2f57aab1f5921f8048363e8f9801cfe4b8f7b0
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2024-02-18 13:04:09 -0500

    FIX: FAQ comments

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,3 +1,8 @@
+// Name: Justin Barlowe, John Davidsion, Nolan Berryhill
+// 02/18/2024
+// Description: This is the FAQ component that will be used to display the FAQ page.
+// File: faq.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
