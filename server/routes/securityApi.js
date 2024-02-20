1d0225dc38b24f576e76d53b8d56f108976ed381
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2024-02-19 19:16:06 -0500

    Added secuirty API file

--- server/routes/securityApi.js
+++ server/routes/securityApi.js
@@ -1,4 +1,4 @@
 // Name: Justin Barlowe
 // Date: 02/19/2024
 // File: server/routes/securityApi.js
-// Purpose: This file contains the security API routes
+// Purpose: This file contains the security API routes, Register, verifyUser, verifySecurityQuestion, and resetPassword.
