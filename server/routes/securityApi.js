0c963bcc366cf6b24d17784eb733f04edb84582e
Jojo <davidson.jojo@yahoo.com>
2024-02-20 23:46:06 -0600

    registerUser and verifyUser APIs completed

--- server/routes/securityApi.js
+++ server/routes/securityApi.js
@@ -1,4 +1,4 @@
-// Name: Justin Barlowe, Nolan Berryhill
+// Name: Justin Barlowe, Nolan Berryhill, John Davidson
 // Date: 02/19/2024
 // File: server/routes/securityApi.js
 // Purpose: This file contains the security API routes, Register, verifyUser, verifySecurityQuestion, and resetPassword.
@@ -7,6 +7,7 @@
 const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 // Verify Security Questions
 router.post("/:email/securityquestions", async (req, res) => {
@@ -68,6 +69,70 @@ router.post("/:email/reset-password", async (req, res) => {
     return res.status(500).json({ status: 500, message: "Internal Server Error", error: err });
   }
 });
+
+// registerUser API
+router.post('/register', async(req, res, next) => {
+
+  // Extract properties from the req.body
+  const { userName, password, email } = req.body;
+
+  // Check if username and password were entered.
+  if(!userName || !password ) {
+    return res.status(400).json({ error: 'Username and password are required.' })
+  }
+
+  try {
+    const hashedPassword = bcrypt.hashSync(password, 10); // Hash the user's plaintext password before storing it in the database.
+    const existingUser = await User.findOne({email: email}); // Check if entered email already exists.
+
+    // If the email is already in use, respond with 404.
+    if (existingUser) {
+      return res.status(404).json({ message: 'This email is already registered under a different account.'})
+    }
+
+    // Create a new user document in the User collection with properties for username, password(hashed), and email.
+    const newUser = new User({
+      userName,
+      password: hashedPassword,
+      email
+    });
+
+    // Save the the new document to the User collection.
+    await newUser.save();
+
+    // Respond with 201 status code and the newly generated data.
+    res.status(201).json({ message: 'New user created', user: newUser });
+
+    // Resolve unhandled errors.
+  } catch (err) {
+    console.error('err', err)
+    return res.status(500).json({ error: 'Internal server error'})
+  }
+})
+
+// VerifyUser API
+router.get('/register/:email', async(req, res, next) => {
+
+  try {
+    // Extract the email from the request parameters.
+    const { email } = req.params;
+    // Find a user with a specified email in the database.
+    const existingUser = await User.findOne({ email: email})
+
+    // If entered email does not exist in the database, respond with a 400 status and json message.
+    if(!existingUser) {
+      console.error('err', err)
+      return res.status(400).json({ message: 'The email you entered does not exist.' })
+    }
+
+    // If the email exists, respond with a 200 status code and send the data of the user.
+    res.status(200).send(existingUser)
+
+  } catch (err) {
+    // If an error occurs during execution, respond with a 500 status code and an error message.
+    res.status(500).json({ message: 'Internal server error'})
+  }
+})
 
 // Export router
 module.exports = router;
